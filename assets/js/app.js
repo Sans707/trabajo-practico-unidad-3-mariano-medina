de3a5c9d41d4b3eb19bec16b29e4e477991062a5
Sans707 <Sonicmariano707@gmail.com>
2026-07-06 18:55:48 -0300

    se agrego el listado de personajes

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+
+const API_URL = "https://thesimpsonsapi.com/api/characters";
+const CDN_URL = "https://cdn.thesimpsonsapi.com/500";
+const contenedor = document.getElementById("cartaPj");
+const mensaje = document.getElementById("mensajeEstado");
+const inputBuscar = document.getElementById("buscarPj");
+let personajes = [];
